9f51b0144c0f907dae52b3c48e718d062054cf8c
Dirk Grunwald <grunwald@cs.colorado.edu>
2021-09-14 20:16:43 -0600

    disable cpplint copyright

--- overflow-undefined.cc
+++ overflow-undefined.cc
@@ -8,13 +8,16 @@
 //
 // The C and C++ language standards say that arithmetic overflow for
 // signed integers is undefined while unsigned integers always wrap
-// around. Specifically, they say ( https://en.cppreference.com/w/cpp/language/operator_arithmetic#Overflows ):
+// around. Specifically, they say
+// (https://en.cppreference.com/w/cpp/language/operator_arithmetic#Overflows ):
 // When signed integer arithmetic operation overflows (the result does
 // not fit in the result type), the behavior is undefined, — the
 // possible manifestations of such an operation include:
 
-// * it wraps around according to the rules of the representation (typically 2's complement),
-// * it traps — on some platforms or due to compiler options (e.g. -ftrapv in GCC and Clang),
+// * it wraps around according to the rules of the representation
+//   (typically 2's complement),
+// * it traps — on some platforms or due to compiler options
+//   (e.g. -ftrapv in GCC and Clang),
 // * it saturates to minimal or maximal value (on many DSPs),
 // * it is completely optimized out by the compiler.
 //
